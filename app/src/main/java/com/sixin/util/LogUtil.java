9e484192340e449e0bc48ee6dd1308a2f55b08f5
unknown <2055205346@qq.com>
2017-07-20 09:48:15 +0800

    初次构建

---
+++ app/src/main/java/com/sixin/util/LogUtil.java
@@ -0,0 +1,31 @@
+package com.sixin.util;
+
+import android.util.Log;
+
+/**
+ * 日志工具类，通过修改该类中DEBUG的值
+ * ，来确定是否需要打印日志。DEBUG = true，
+ * 打印日志，否则不打印日志
+ *
+ * @author 周文涛
+ */
+
+public class LogUtil {
+
+    /**
+     * 用于控制日志是否输出的常量
+     * */
+    private static final  boolean DEBUG = true;
+
+    /**
+     * 打印debug日志的泛型方法
+     *@param clazz 打印的日志所在的类的类类型对象
+     *@param msg 需要打印的日志内容
+     * */
+    public static  void d(Class clazz,String msg){
+        if(DEBUG){
+            Log.d(clazz.getName(),msg);
+        }
+    }
+
+}
